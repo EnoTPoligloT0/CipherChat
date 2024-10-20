2b1594186e833f1f8c451723c0c928afd03a7d0c
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-20 23:06:51 +0200

    Extended method with shift and language variable

--- backend/CipherChat.Services/Interfaces/ICipherService.cs
+++ backend/CipherChat.Services/Interfaces/ICipherService.cs
@@ -2,6 +2,6 @@ namespace CipherChat.Domain.Interfaces;
 
 public interface ICipherService
 {
-    public string Encrypt(string plainText);
-    public string Decrypt(string cipherText);
+    public string Encrypt(string plainText, int shift, string language);
+    public string Decrypt(string cipherText, int shift, string language);
 }
