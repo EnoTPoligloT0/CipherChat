f1ee9b8f12f7b0dfac9d91d539f5262e9f6d5b8f
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-21 22:34:06 +0200

    Tests for vigenere cipher algorithm

---
+++ backend/CipherChat.Tests/Ciphers.Tests/VigenereCipherTests.cs
@@ -0,0 +1,35 @@
+﻿using CipherChat.Ciphers.VigenereCipher;
+using Xunit;
+using FluentAssertions;
+
+namespace CipherChat.Ciphers.Tests
+{
+    public class VigenereCipherServiceTests
+    {
+        [Theory]
+        [InlineData("hello", "secret", "zincs", "EN")]
+        [InlineData("world", "abc", "wptle", "EN")]
+        [InlineData("Witaj", "klucz", "Ętncg", "PL")] // Mixed case for Polish
+        public void Encrypt_ShouldReturnEncryptedText(string plainText, string key, string expectedCipherText, string language)
+        {
+            var cipherService = new VigenereCipherService(language);
+
+            var result = cipherService.Encrypt(plainText, key);
+
+            result.Should().Be(expectedCipherText);
+        }
+
+        [Theory]
+        [InlineData("zincs", "secret", "hello", "EN")]
+        [InlineData("wptle", "ABC", "world", "EN")]
+        [InlineData("Ętncg", "klucz", "Witaj", "PL")] // Mixed case for Polish
+        public void Decrypt_ShouldReturnDecryptedText(string cipherText, string key, string expectedPlainText, string language)
+        {
+            var cipherService = new VigenereCipherService(language);
+
+            var result = cipherService.Decrypt(cipherText, key);
+
+            result.Should().Be(expectedPlainText);
+        }
+    }
+}
