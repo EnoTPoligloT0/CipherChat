19174ec6c6c7fd5fec2f3b9eac9b16ca87301197
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-24 02:46:49 +0200

    Complete style overhaul

--- frontend/src/components/WaitingRoom.tsx
+++ frontend/src/components/WaitingRoom.tsx
@@ -1,14 +1,42 @@
-import {Heading, Input, Text} from "@chakra-ui/react";
+import { Heading, Input, Text, Button, Box, Flex } from "@chakra-ui/react";
 
 function WaitingRoom() {
     return (
-        <form className="max-w-sm w-full bg-gray-400 p-8 rounded shadow-lg">
-            <Heading> Cipher Chat</Heading>
-            <div className="mb-4">
-                <Text fontSize={"sm"}>Username</Text>
-                <Input name={"userName"} placeholder={"Enter your name"}></Input>
-            </div>
-        </form>
+        <Flex className="min-h-screen bg-background-gray items-center justify-center font-mono" >
+            <Box className="max-w-lg w-full bg-gray-800 p-12 rounded-lg shadow-xl">
+                <Heading className="text-cool-white mb-4 text-center text-5xl font-extrabold">
+                    Cipher Chat
+                </Heading>
+                <Text className="text-cool-white text-center mb-6 text-lg">
+                    Join the conversation
+                </Text>
+                <form>
+                    <div className="mb-8">
+                        <Text className="text-cool-white font-medium mb-2">Username</Text>
+                        <Input
+                            name="userName"
+                            placeholder="Enter your name"
+                            className="border border-cool-white text-cool-white placeholder-opacity-70 bg-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-soft-teal transition p-4"
+                            size="lg"
+                        />
+                    </div>
+                    <div className="mb-8">
+                        <Text className="text-cool-white font-medium mb-2">Chatroom name</Text>
+                        <Input
+                            name="chatRoom"
+                            placeholder="Enter chatroom name"
+                            className="border border-cool-white text-cool-white placeholder-opacity-70 bg-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-soft-teal transition p-4"
+                            size="lg"
+                        />
+                    </div>
+                    <Button
+                        className="w-full bg-soft-teal text-gray-800 hover:bg-opacity-90 transition rounded-md shadow-md py-3 text-lg"
+                        type="submit">
+                        Join Room
+                    </Button>
+                </form>
+            </Box>
+        </Flex>
     );
 }
 
