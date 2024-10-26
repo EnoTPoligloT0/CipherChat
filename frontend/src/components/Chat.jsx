14d195c881ad6eac6f12f54e97dc1502b69872b0
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-26 23:04:27 +0200

    Send Message function and simple styling

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,10 +1,18 @@
-import { Heading } from "@chakra-ui/react";
+import { Button, Heading, Input } from "@chakra-ui/react";
 import { CloseButton } from "./ui/close-button.tsx";
 import { Message } from "./Message.jsx";
+import { useState } from "react";
 
-const Chat = ({ messages, chatRoom, closeChat }) => {
+const Chat = ({ messages, chatRoom, sendMessage, closeChat }) => {
+    const [message, setMessage] = useState(""); // Initialized with an empty string
+
+    const onSendMessage = () => {
+        sendMessage(message);
+        setMessage("");
+    };
+
     return (
-        <div className="w-1/2 p-8 rounded shadow-lg">
+        <div className="w-1/2 p-8 bg-gray-800 rounded shadow-lg">
             <div className="flex flex-row justify-between mb-5">
                 <Heading size={"lg"} className="text-2xl font-bold leading-tight">
                     {chatRoom}
@@ -17,8 +25,19 @@ const Chat = ({ messages, chatRoom, closeChat }) => {
                     <Message messageInfo={messageInfo} key={index} />
                 ))}
             </div>
+
+            <div className="flex gap-3">
+                <Input
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    placeholder={"Write a message"}
+                />
+                <Button colorScheme="blue" onClick={onSendMessage}>Send</Button>
+            </div>
+
         </div>
     );
 };
 
-export default Chat; // Ensure you have a default export
+export default Chat;
