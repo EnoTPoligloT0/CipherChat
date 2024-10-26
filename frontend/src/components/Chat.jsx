ed5c374ca6e4157bad84c3f510de32cd9c42891f
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-26 21:19:22 +0200

    User info on connection

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,16 +1,24 @@
-import {Heading} from "@chakra-ui/react";
-import {CloseButton} from "./ui/close-button.tsx";
+import { Heading } from "@chakra-ui/react";
+import { CloseButton } from "./ui/close-button.tsx";
+import { Message } from "./Message.jsx";
 
-export const Chat = ({chatRoom, closeChat}) => {
+const Chat = ({ messages, chatRoom, closeChat }) => {
     return (
-        <div className="w-1/2 soft-teal p-8 rounded shadow-lg">
+        <div className="w-1/2 p-8 rounded shadow-lg">
             <div className="flex flex-row justify-between mb-5">
-                <Heading size={"lg"} className="text-2xl font-bold leading-tight">{chatRoom}</Heading>
+                <Heading size={"lg"} className="text-2xl font-bold leading-tight">
+                    {chatRoom}
+                </Heading>
                 <CloseButton onClick={closeChat} />
             </div>
 
+            <div className="flex flex-col overflow-auto scroll scroll-smooth h-96 gap-3 pb-3">
+                {messages.map((messageInfo, index) => (
+                    <Message messageInfo={messageInfo} key={index} />
+                ))}
+            </div>
         </div>
+    );
+};
 
-    )
-
-}
+export default Chat; // Ensure you have a default export
