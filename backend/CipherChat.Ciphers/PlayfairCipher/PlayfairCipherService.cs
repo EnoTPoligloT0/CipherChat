221b0da553b4b8ae5f3db0d02b0b18e037bb3f10
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-28 23:11:16 +0100

    Playfair cipher algorithm

---
+++ backend/CipherChat.Ciphers/PlayfairCipher/PlayfairCipherService.cs
@@ -0,0 +1,127 @@
+using System.Text;
+
+namespace CipherChat.Ciphers
+{
+    public class PlayfairCipher
+    {
+        public static string Encrypt(string plaintext, string keyword, string language)
+        {
+            var alphabet = AlphabetProvider.GetAlphabet(language);
+            var matrix = CreateMatrix(keyword, alphabet);
+            return ProcessText(plaintext, matrix, true);
+        }
+
+        public static string Decrypt(string ciphertext, string keyword, string language)
+        {
+            var alphabet = AlphabetProvider.GetAlphabet(language);
+            var matrix = CreateMatrix(keyword, alphabet);
+            string decryptedText = ProcessText(ciphertext, matrix, false);
+            return RemoveTrailingPadding(decryptedText);
+        }
+
+        public static char[,] CreateMatrix(string keyword, string alphabet)
+        {
+            int gridSize = alphabet.Length > 26 ? 6 : 5;
+            var uniqueChars = new HashSet<char>();
+
+            foreach (char c in keyword)
+            {
+                if (alphabet.Contains(c))
+                {
+                    uniqueChars.Add(c);
+                }
+            }
+
+            foreach (char c in alphabet)
+            {
+                uniqueChars.Add(c);
+            }
+
+            var matrix = new char[gridSize, gridSize];
+            var distinctChars = uniqueChars.Take(gridSize * gridSize).ToArray();
+
+            for (int i = 0; i < distinctChars.Length; i++)
+            {
+                matrix[i / gridSize, i % gridSize] = distinctChars[i];
+            }
+
+            return matrix;
+        }
+
+        private static string ProcessText(string text, char[,] matrix, bool encrypt)
+        {
+            int gridSize = matrix.GetLength(0);
+            var formattedText = FormatText(text);
+            var result = new StringBuilder();
+
+            for (int i = 0; i < formattedText.Length; i += 2)
+            {
+                char a = formattedText[i];
+                char b = formattedText[i + 1];
+                (int rowA, int colA) = FindPosition(matrix, a);
+                (int rowB, int colB) = FindPosition(matrix, b);
+
+                if (rowA == rowB) 
+                {
+                    result.Append(matrix[rowA, (colA + (encrypt ? 1 : gridSize - 1)) % gridSize]);
+                    result.Append(matrix[rowB, (colB + (encrypt ? 1 : gridSize - 1)) % gridSize]);
+                }
+                else if (colA == colB) 
+                {
+                    result.Append(matrix[(rowA + (encrypt ? 1 : gridSize - 1)) % gridSize, colA]);
+                    result.Append(matrix[(rowB + (encrypt ? 1 : gridSize - 1)) % gridSize, colB]);
+                }
+                else
+                {
+                    result.Append(matrix[rowA, colB]);
+                    result.Append(matrix[rowB, colA]);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private static (int, int) FindPosition(char[,] matrix, char character)
+        {
+            int gridSize = matrix.GetLength(0);
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int col = 0; col < gridSize; col++)
+                {
+                    if (matrix[row, col] == character)
+                        return (row, col);
+                }
+            }
+
+            throw new ArgumentException($"Character '{character}' not found in matrix.");
+        }
+
+        private static string FormatText(string text)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i += 2)
+            {
+                char a = text[i];
+                char b = (i + 1 < text.Length && text[i + 1] != a) ? text[i + 1] : 'x';
+
+                sb.Append(a);
+                sb.Append(b);
+            }
+
+            if (sb.Length % 2 != 0)
+                sb.Append('x'); 
+
+            return sb.ToString();
+        }
+
+        private static string RemoveTrailingPadding(string text)
+        {
+            if (text.EndsWith("x") && !text.EndsWith("xx"))
+            {
+                text = text.Remove(text.Length - 1, 1);
+            }
+            return text;
+        }
+    }
+}
