98a8577e009c92ce07e1b63f2a50fef28fee486e
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-29 14:28:11 +0100

    Variables to var

--- backend/CipherChat.Ciphers/CaesarCipher/CaesarCipherService.cs
+++ backend/CipherChat.Ciphers/CaesarCipher/CaesarCipherService.cs
@@ -7,22 +7,22 @@ namespace CipherChat.Ciphers.CaesarCipher
     {
         public string Encrypt(string plainText, string key, string language)
         {
-            int shift = Convert.ToInt32(key); 
-            string alphabet = AlphabetProvider.GetAlphabet(language);
+            var shift = Convert.ToInt32(key); 
+            var alphabet = AlphabetProvider.GetAlphabet(language);
             return ProcessText(plainText, shift, alphabet, true);
         }
 
         public string Decrypt(string cipherText, string key, string language)
         {
-            int shift = Convert.ToInt32(key); 
-            string alphabet = AlphabetProvider.GetAlphabet(language);
+            var shift = Convert.ToInt32(key); 
+            var alphabet = AlphabetProvider.GetAlphabet(language);
             return ProcessText(cipherText, shift, alphabet, false);
         }
 
         private string ProcessText(string text, int shift, string alphabet, bool isEncryption)
         {
-            StringBuilder result = new StringBuilder();
-            foreach (char character in text)
+            var result = new StringBuilder();
+            foreach (var character in text)
             {
                 result.Append(ProcessCharacter(character, shift, alphabet, isEncryption));
             }
@@ -31,13 +31,13 @@ namespace CipherChat.Ciphers.CaesarCipher
 
         private char ProcessCharacter(char character, int shift, string alphabet, bool isEncryption)
         {
-            char targetChar = char.ToLower(character);
+            var targetChar = char.ToLower(character);
             if (alphabet.Contains(targetChar))
             {
-                int alphabetIndex = alphabet.IndexOf(targetChar);
-                int shiftAmount = isEncryption ? shift : -shift;
-                int newCharIndex = (alphabetIndex + shiftAmount + alphabet.Length) % alphabet.Length;
-                char resultChar = alphabet[newCharIndex];
+                var alphabetIndex = alphabet.IndexOf(targetChar);
+                var shiftAmount = isEncryption ? shift : -shift;
+                var newCharIndex = (alphabetIndex + shiftAmount + alphabet.Length) % alphabet.Length;
+                var resultChar = alphabet[newCharIndex];
 
                 return char.IsUpper(character) ? char.ToUpper(resultChar) : resultChar;
             }
