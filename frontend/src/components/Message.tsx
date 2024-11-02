0292afc6913edb6d4c02eac8eea63ec7f148b027
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-11-02 23:54:20 +0100

    Refactor to tsx

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -1,13 +1,14 @@
-export const Message = ({messageInfo}) => {
+interface MessageProps {
+    messageInfo: { userName: string; message: string };
+}
+
+export const Message: React.FC<MessageProps> = ({ messageInfo }) => {
     return (
         <div className="w-fit">
             <span className="text-sm text-cool-white">{messageInfo.userName}</span>
             <div className="p-2 bg-cool-white text-teal-950 rounded-lg shadow-md">
-                {console.log(messageInfo)}
                 {messageInfo.message}
             </div>
         </div>
-    )
-
-
-}
+    );
+};
