0f827e95331035684c1a6ea423f2c9f3331649a7
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-11-04 22:31:38 +0100

    Cipher options modal

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,7 +1,18 @@
-import { Button, Heading, Input } from "@chakra-ui/react";
+import {
+    Button,
+    Heading,
+    Input,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure
+} from "@chakra-ui/react";
 import { CloseButton } from "./ui/close-button";
 import { Message } from "./Message";
-import { useState } from "react";
+import { useState, useCallback } from "react";
+import { motion } from "framer-motion";
 
 interface ChatProps {
     messages: { userName: string; message: string }[];
@@ -15,20 +26,32 @@ const Chat: React.FC<ChatProps> = ({ messages, chatRoom, sendMessage, closeChat 
     const [cipherType, setCipherType] = useState("");
     const [language, setLanguage] = useState("EN");
     const [key, setKey] = useState("");
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const onSendMessage = () => {
+    const onSendMessage = useCallback(() => {
         if (message.trim()) {
             sendMessage(message, cipherType, language, key);
             setMessage("");
             setKey("");
             setCipherType("");
         }
+    }, [message, cipherType, language, key, sendMessage]);
+
+    const handleCipherSelection = (cipher: string) => {
+        if (cipher !== "Soon") {
+            setCipherType(cipher);
+            if (cipher === "Polibius") {
+                setKey("1");
+            } else {
+                setKey("");
+            }
+        }
     };
 
     return (
         <div className="w-1/2 p-8 bg-gray-800 rounded shadow-lg">
             <div className="flex justify-between items-center mb-5">
-                <Heading size="lg" className="text-2xl font-bold text-white">
+                <Heading size="lg" className="text-2xl font-bold text-cool-white">
                     {chatRoom}
                 </Heading>
                 <CloseButton onClick={closeChat} />
@@ -40,56 +63,79 @@ const Chat: React.FC<ChatProps> = ({ messages, chatRoom, sendMessage, closeChat 
                 ))}
             </div>
 
-            <div className="flex gap-3 mt-5">
-                <div className="flex flex-col">
-                    <label className="text-sm text-gray-300">Cipher Type</label>
-                    <select
-                        value={cipherType}
-                        onChange={(e) => setCipherType(e.target.value)}
-                        className="bg-gray-700 text-white p-2 rounded"
-                    >
-                        <option value="">Select cipher</option>
-                        <option value="Caesar">Caesar</option>
-                        <option value="Vigenere">Vigenere</option>
-                        <option value="Playfair">Playfair</option>
-                        <option value="Polibius">Polibius</option>
-                    </select>
-                </div>
-                <div className="flex flex-col">
-                    <label className="text-sm text-gray-300">Language</label>
-                    <select
-                        value={language}
-                        onChange={(e) => setLanguage(e.target.value)}
-                        className="bg-gray-700 text-white p-2 rounded"
-                    >
-                        <option value="EN">English</option>
-                        <option value="PL">Polish</option>
-                    </select>
-                </div>
-                <div className="flex flex-col flex-grow">
-                    <label className="text-sm text-gray-300">Key (optional)</label>
-                    <Input
-                        type="text"
-                        value={key}
-                        onChange={(e) => setKey(e.target.value)}
-                        placeholder="Enter key"
-                        className="bg-gray-700 text-white"
-                    />
-                </div>
-                <div className="flex flex-col flex-grow">
-                    <label className="text-sm text-gray-300">Message</label>
-                    <Input
-                        type="text"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        placeholder="Write a message"
-                        className="bg-gray-700 text-white"
-                    />
-                </div>
-                <Button colorScheme="blue" onClick={onSendMessage}>
+            <div className="flex gap-3 mt-5 items-center">
+                <Input
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    placeholder="Write a message"
+                    className="bg-gray-700 text-cool-white flex-grow"
+                />
+                <Button className="bg-soft-teal text-cool-white" onClick={onSendMessage}>
                     Send
                 </Button>
+                <Button className="bg-teal-dark text-cool-white" onClick={onOpen}>
+                    Cipher Options
+                </Button>
             </div>
+
+            <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                <ModalOverlay />
+                <ModalContent bg="background-gray" borderRadius="md" maxW="sm" p={4}>
+                    <ModalCloseButton color="cool-white" />
+                    <ModalBody display="flex" flexDirection="column" alignItems="center" gap={3}>
+                        <Heading size="md" color="cool-white" mb={3}>
+                            Select a Cipher
+                        </Heading>
+                        <div className="grid grid-cols-3 gap-3">
+                            {["Caesar", "Vigenere", "Playfair", "Polibius", "Soon", "Soon"].map((cipher, index) => (
+                                <motion.div
+                                    whileHover={{ scale: 1.1 }}
+                                    key={index}
+                                    className={`flex items-center justify-center text-gray-800 ${
+                                        cipher === cipherType ? 'bg-teal-dark' : 'bg-text-primary'
+                                    } text-white rounded p-4 cursor-pointer transition-colors duration-200`}
+                                    onClick={() => handleCipherSelection(cipher)}
+                                >
+                                    {cipher}
+                                </motion.div>
+                            ))}
+                        </div>
+                        <div className="flex gap-3 mt-4">
+                            {["EN", "PL"].map((lang) => (
+                                <Button
+                                    key={lang}
+                                    bg={language === lang ? "teal-dark" : "text-primary"}
+                                    color={language === lang ? "white" : "gray.400"}
+                                    _hover={{ bg: "" }}
+                                    flex="1"
+                                    onClick={() => setLanguage(lang)}
+                                >
+                                    {lang}
+                                </Button>
+                            ))}
+                        </div>
+                        {cipherType !== "Polibius" && (
+                            <Input
+                                placeholder={`Enter key (${cipherType === "Caesar" ? "Numbers only" : "Letters only"})`}
+                                value={key}
+                                onChange={(e) => setKey(e.target.value)}
+                                bg="gray.800"
+                                color="white"
+                                mt={4}
+                                inputMode={cipherType === "Caesar" ? "numeric" : "text"}
+                                pattern={cipherType === "Caesar" ? "[0-9]*" : "[A-Za-z]*"}
+                            />
+                        )}
+                        <Button
+                            className="mt-4 bg-teal-dark text-cool-white"
+                            onClick={onClose}
+                        >
+                            OK
+                        </Button>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
         </div>
     );
 };
