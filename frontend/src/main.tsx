1f1231177fc75bb160dad87c0203c4d1fa3dc507
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-11-04 22:30:39 +0100

    Wrap to custom theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
+import { ChakraProvider } from "@chakra-ui/react";
+import ChakraTheme from "../chakra-theme.ts"
 import App from './App.tsx'
-import {Provider} from "./components/ui/provider.tsx";
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Provider>
+        <ChakraProvider theme={ChakraTheme}>
             <App/>
-        </Provider>
+        </ChakraProvider>
     </StrictMode>,
 )
